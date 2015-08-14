ce27ce716a84b73d9a2325fbd8826940d263d2a8
ndphu <ngdacphu.khtn@gmail.com>
2015-08-14 15:37:04 +0700

    .

--- custom-cli/update_hosts.sh
+++
@@ -1,7 +0,0 @@
-hosts_file=/etc/hosts
-docker ps | tail -n +2 | awk 'BEGIN {FS = " +"}; {print $(NF-1)}' | while read container_name; 
-do 
-    address=$(docker inspect --format "{{.NetworkSettings.IPAddress}}" $container_name); 
-    sed -i "/$container_name/d" $hosts_file; 
-    echo $address $container_name >> $hosts_file; 
-done
