e9a0d903f12025dd36ffd2aee07532984d96c362
Phu Nguyen <ngdacphu.khtn@gmail.com>
2015-08-21 00:08:15 +0700

    added keys and update utils script

--- utils/update_hosts.sh
+++ utils/update_hosts.sh
@@ -1,5 +1,5 @@
 hosts_file=/etc/hosts
-docker ps | tail -n +2 | awk 'BEGIN {FS = " +"}; {print $(NF-1)}' | while read container_name; 
+docker ps | tail -n +2 | awk 'BEGIN {FS = " +"}; {print $(NF)}' | while read container_name; 
 do 
     echo $container_name
     address=$(docker inspect --format "{{.NetworkSettings.IPAddress}}" $container_name); 
