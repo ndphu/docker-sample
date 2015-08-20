c12b08cc481c09fe0107a0ec37bc67d6d55be4ab
ndphu <ngdacphu.khtn@gmail.com>
2015-08-20 18:52:48 +0700

    Allow auto added key

--- utils/update_hosts.sh
+++ utils/update_hosts.sh
@@ -1,6 +1,7 @@
 hosts_file=/etc/hosts
-docker ps | tail -n +2 | awk 'BEGIN {FS = " +"}; {print $(NF)}' | while read container_name; 
+docker ps | tail -n +2 | awk 'BEGIN {FS = " +"}; {print $(NF-1)}' | while read container_name; 
 do 
+    echo $container_name
     address=$(docker inspect --format "{{.NetworkSettings.IPAddress}}" $container_name); 
     sed -i "/$container_name/d" $hosts_file; 
     echo $address $container_name >> $hosts_file; 
